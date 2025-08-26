c143ed5611879bb40d51b2a4f6b9f424676c5a26
sy.van <vansy151101@gmail.com>
2025-08-27 00:14:19 +0700

    update sysconf

--- src/news_generator/sysconf/base.py
+++ src/news_generator/sysconf/base.py
@@ -1,3 +1,4 @@
+# %%
 import os, yaml, collections
 
 def load_yaml_file(file_path):
@@ -7,10 +8,11 @@ _module_filepath = os.path.dirname(os.path.abspath(__file__))
 _secret_api_key_fp = os.path.join(_module_filepath, "secret_api_keys.yaml")
 _secret_news_sources_fp = os.path.join(_module_filepath, "secret_news_sources.yaml")
 
-api_key_list = load_yaml_file(_secret_api_key_fp).get('api_key_list', [])
-news_source_dict = load_yaml_file(_secret_news_sources_fp).get('news_source_dict', {})
+api_key_dict = load_yaml_file(_secret_api_key_fp).get('api_key_dict', {})
+news_source_list = load_yaml_file(_secret_news_sources_fp).get('news_source_list', [])
 
 conf = collections.OrderedDict()
 
-conf["api_key_list"] = api_key_list
-conf["news_source_dict"] = news_source_dict
+conf["api_key_dict"] = api_key_dict
+conf["news_source_list"] = news_source_list
+# %%
