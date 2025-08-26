d9cdd7cd5ac471fabb5d42d225faf4ff876210d7
sy.van <vansy151101@gmail.com>
2025-08-27 00:40:27 +0700

    update sysconf

--- src/news_generator/sysconf/base.py
+++ src/news_generator/sysconf/base.py
@@ -1,18 +1,26 @@
 # %%
 import os, yaml, collections
+from pathlib import Path
 
 def load_yaml_file(file_path):
     with open(file_path, 'r') as file:
         return yaml.safe_load(file)
-_module_filepath = os.path.dirname(os.path.abspath(__file__))
-_secret_api_key_fp = os.path.join(_module_filepath, "secret_api_keys.yaml")
-_secret_news_sources_fp = os.path.join(_module_filepath, "secret_news_sources.yaml")
+
+conf = collections.OrderedDict()
+
+conf["_glb_prj_path"] = Path(__file__).resolve().parents[3].as_posix()
+
+_sysconf_fp = os.path.join(conf["_glb_prj_path"], "src", "news_generator", "sysconf")
+_secret_api_key_fp = os.path.join(_sysconf_fp, "secret_api_keys.yaml")
+_secret_news_sources_fp = os.path.join(_sysconf_fp, "secret_news_sources.yaml")
 
 api_key_dict = load_yaml_file(_secret_api_key_fp).get('api_key_dict', {})
 news_source_list = load_yaml_file(_secret_news_sources_fp).get('news_source_list', [])
 
-conf = collections.OrderedDict()
 
 conf["api_key_dict"] = api_key_dict
 conf["news_source_list"] = news_source_list
+
+conf["data_folder_path"] = os.path.join(conf["_glb_prj_path"], "data")
+
 # %%
