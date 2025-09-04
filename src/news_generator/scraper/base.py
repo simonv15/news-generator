5a953914d1e57cfdf65eb54182ee6a5a2f3cf0ce
sy.van <vansy151101@gmail.com>
2025-09-04 22:39:06 +0700

    update scraper

--- src/news_generator/scraper/base.py
+++ src/news_generator/scraper/base.py
@@ -3,7 +3,21 @@ import os, sys
 from firecrawl import FirecrawlApp
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from sysconf import base as conf
+from sysconf.base import conf
+
+def scrape_content(url, formats, actions):
+    app = FirecrawlApp(api_key=conf["api_key_dict"].get("FIRECRAWL_API_KEY"))
+    try:
+        scrape_result = app.scrape(
+            url=url,
+            formats=formats,
+            proxy="auto",
+            actions=actions
+        )
+        if scrape_result:
+            return scrape_result
+    except Exception as e:
+        print(f" {url}: {e}")
 
 def get_news_urls():
     pass
